70d0c677e6723c6213930ca7ec16c406f50c13a8
Miguel Ramírez <mramirez_juarez@outlook.com>
2022-10-11 13:18:20 -0500

    First Commit. Falta por región

---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from "rxjs/operators";
+import { Country } from '../interfaces/pais.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private apiURL: string = 'https://restcountries.com/v3.1';
+
+  constructor(private http: HttpClient) { }
+
+  buscarPais(termino: string): Observable<Country[]> {
+    const url = `${this.apiURL}/name/${termino}`;
+    return this.http.get<Country[]>(url);
+
+    // return this.http.get(url).pipe(
+    //   catchError(err => of(['uwu']))
+    // );
+  }
+
+  buscarCapital(termino: string): Observable<Country[]> {
+    const url = `${this.apiURL}/capital/${termino}`;
+    return this.http.get<Country[]>(url);
+  }
+
+  getPaisPorAlpha(id: string): Observable<Country> {
+    const url = `${this.apiURL}/alpha/${id}`;
+    return this.http.get<Country>(url);
+  }
+}
